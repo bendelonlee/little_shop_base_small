a0f463bac93bbf30e50974292045a4d2b69bc09f
Ben Lee <bendelonlee@gmail.com>
2019-01-03 11:13:47 -0700

    Sets up discounts to affect order_item subtotals

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,12 +6,53 @@ class OrderItem < ApplicationRecord
     only_integer: false,
     greater_than_or_equal_to: 0
   }
+
+  validates :amount_discounted, presence: true, numericality: {
+    only_integer: false,
+    greater_than_or_equal_to: 0
+  }
+
   validates :quantity, presence: true, numericality: {
     only_integer: true,
     greater_than_or_equal_to: 1
   }
 
+  before_validation :set_amount_discounted
+
+  def applicable_discount
+    return nil if !item || item.user.discounts.empty?
+    order_amount = case item.user.discounts.first.discount_type
+                  when "percent"
+                    quantity
+                  when "dollar"
+                    undiscounted_subtotal
+                  end
+    item.user.discounts.where("discounts.min_amount <= ?", order_amount)
+                       .order(value_off: :desc)
+                       .limit(1).first
+  end
+
+  def set_amount_discounted
+    ad = applicable_discount
+    unless ad
+      self.amount_discounted = 0
+      return
+    end
+    amount_to_discount = case ad.discount_type
+                        when "percent"
+                          0.01 * ad.value_off * undiscounted_subtotal
+                        when "dollar"
+                          ad.value_off
+                        end
+    self.amount_discounted = amount_to_discount
+  end
+
   def subtotal
+    undiscounted_subtotal - amount_discounted
+  end
+
+  def undiscounted_subtotal
     quantity * price
   end
+
 end
--- spec/factories/order_item.rb
+++ spec/factories/order_item.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     sequence(:quantity) { |n| ("#{n}".to_i+1)*2 }
     sequence(:price) { |n| ("#{n}".to_i+1)*1.5 }
     fulfilled { false }
+    amount_discounted { 0 }
   end
   factory :fulfilled_order_item, parent: :order_item do
     fulfilled { true }
